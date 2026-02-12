989373c3773d45a431178a1da395dedaa6bc3380
Ridhushni Balaji <ridhushni.balaji@gmail.com>
2026-02-11 21:51:56 -0600

    add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Ridhushni Balaji" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I am a freshman from Texas. I enjoy math and problem solving, and I like painting, arts and crafts, and playing basketball.")
 
 if __name__ == "__main__":
     main()
